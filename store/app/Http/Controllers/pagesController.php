5a100eb47f7a9018bb713b44ba0da1c349d0c2f9
dakshinasingh <dakshinasingh25@gmail.com>
2023-03-16 12:34:57 +0530

    routes controller

---
+++ store/app/Http/Controllers/pagesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pagesController extends Controller
+{
+    public function home(){
+		return view('pages/home');
+	}
+	public function cart(){
+		return view('pages/cart');
+	}
+
+}
--- store/routes/web.php
+++ store/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\pagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[pagesController::class,'Home'])->name('home');
+Route::get('cart',[pagesController::class,'Cart'])->name('cart');
