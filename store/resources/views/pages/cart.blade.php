a27dd8aff62ae93ee58dd0a645c08290019f393e
dakshinasingh <dakshinasingh25@gmail.com>
2023-05-10 11:20:51 +0530

    cart page setup

--- store/resources/views/pages/cart.blade.php
+++ store/resources/views/pages/cart.blade.php
@@ -1,5 +1,54 @@
 @extends('layouts.master')
 @section('title','cart')
 @section('content')
-	cart blade page
+	<header class="page-header">
+		<h1>Cart</h1>
+		<h3 class="cart-amount">$999</h3>
+	</header>
+	<main class="cart-page">
+		<div class="container">
+			<div class="cart-table">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Product</th>
+							<th>Color</th>
+							<th>Price</th>
+							<th>Quantity</th>
+							<th>Total</th>
+							<th>Product</th>
+						</tr>
+					</thead>
+					<tbody>
+						@if(session()->has('cart')&& count(session()->get('cart')) > 0)
+						@foreach(session()->get('cart') as $key => $item)
+							<tr>
+								<td>
+									<a href="{{route('product', $item['product']['id'])}}" class="cart-item-title">
+										<img src="{{asset('storage/'. $item['product']['image'])}}" alt="">
+										<p>{{$item['product']['title']}}</p>
+									</a>
+								</td>
+								<td>{{$item['color']['name']}}</td>
+								<td>{{$item['product']['price']}}</td>
+								<td>{{$item['quantity']}}</td>
+								<td>99</td>
+								<td>
+									<form action="" method="post">
+										@csrf
+										<button type="submit" class="btn btn-primary">X</button>
+									</form>
+								</td>
+							</tr>
+						@endforeach
+						else
+						<tr>
+							<td colspan="6" class="empty-cart">Your Cart is empty</td>
+						</tr>
+						@endif
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</main>
 @endsection
